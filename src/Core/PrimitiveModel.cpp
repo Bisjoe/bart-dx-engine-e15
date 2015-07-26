2a4d6fed0ebbe547487e4ae710c2bbfebab2ff1f
Nick <1430047@apps.bart.qc.ca>
2015-07-25 22:42:41 -0400

    Changed some of the values in the Primitive model to remove warnings and the poop that appeared in my stuff.

--- src/Core/PrimitiveModel.cpp
+++ src/Core/PrimitiveModel.cpp
@@ -21,7 +21,7 @@ PrimitiveModel::PrimitiveModel(PrimitiveModel_Type type)
 	switch (type)
 	{
 	case PrimitiveModel::CYLINDER:
-		HR(D3DXCreateCylinder(gD3DDevice, 1.f, 1.f, 6.f, 20, 20, &mesh, 0));
+		HR(D3DXCreateCylinder(gD3DDevice, 1.f, 1.f, 4.f, 20, 20, &mesh, 0));
 		break;
 	case PrimitiveModel::CUBE:
 		HR(D3DXCreateBox(gD3DDevice, 1.f, 1.f, 1.f, &mesh, 0));
@@ -33,11 +33,7 @@ PrimitiveModel::PrimitiveModel(PrimitiveModel_Type type)
 		HR(D3DXCreateTeapot(gD3DDevice, &mesh, 0));
 		break;
 	case PrimitiveModel::CONE:
-<<<<<<< HEAD
-		HR(D3DXCreateCylinder(gD3DDevice, 0, 1.f, 6.f, 20, 20, &mesh, 0));
-=======
-		HR(D3DXCreateCylinder(gD3DDevice, 1.f, 0.f, 6.f, 20.f, 20.f, &mesh, 0));
->>>>>>> 91e7d12d6601144b34135b25038acf4e4c4d417e
+		HR(D3DXCreateCylinder(gD3DDevice, 1.f, 0.f, 6.f, 20, 20, &mesh, 0));
 		break;
 	default:
 		break;
